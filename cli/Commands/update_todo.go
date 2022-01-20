cf88ff5ee8254cf65f0fcad53a349c763509a1f5
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-20 15:29:39 -0300

    feat: update todo by command

---
+++ cli/Commands/update_todo.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/Vivi3008/todoCLI/cli/commom"
+	"github.com/Vivi3008/todoCLI/domain/usecase"
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+func Update(usecase usecase.TodoUsecase) *cobra.Command {
+	addCmd := &cobra.Command{
+		Use:   "update",
+		Short: "Update todo to done or pending",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println(chalk.Red, "Please type to do ID to list")
+			} else {
+				id := args[0]
+				todo, err := usecase.UpdateTodo(id)
+
+				if err != nil {
+					fmt.Println(chalk.Red, err)
+				}
+
+				commom.PrintTodo(todo)
+			}
+		},
+	}
+	return addCmd
+}
