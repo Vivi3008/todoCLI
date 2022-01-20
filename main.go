e056cd557cb034522671d7dae5aa765fc1c98437
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-20 17:46:35 -0300

    feat: add build

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"github.com/Vivi3008/todoCLI/cli/cmd"
 	"github.com/Vivi3008/todoCLI/domain/usecase"
 	"github.com/Vivi3008/todoCLI/store"
-	"github.com/Vivi3008/todoCLI/task/cmd"
 )
 
 var todoStore = store.NewTodoStore()
