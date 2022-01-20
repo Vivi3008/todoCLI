b0bc8ee662daf9da9ad2a4e5cefaa4933a790ac9
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-20 13:04:37 -0300

    fix: remove extra spaces in last word

--- cli/Commands/add_todo.go
+++ cli/Commands/add_todo.go
@@ -6,14 +6,10 @@ import (
 	"github.com/Vivi3008/todoCLI/cli/commom"
 	"github.com/Vivi3008/todoCLI/domain"
 	"github.com/Vivi3008/todoCLI/domain/usecase"
-	"github.com/Vivi3008/todoCLI/store"
 	"github.com/spf13/cobra"
 )
 
-var todoStore = store.NewTodoStore()
-var todoUsecase = usecase.NewTodoUsecase(todoStore)
-
-func Add() *cobra.Command {
+func Add(usecase usecase.TodoUsecase) *cobra.Command {
 	addCmd := &cobra.Command{
 		Use:   "add",
 		Short: "Add a task to do",
@@ -22,9 +18,15 @@ func Add() *cobra.Command {
 				fmt.Println("Need todo description")
 			}
 			var description string
-			for _, txt := range args {
+			for i, txt := range args {
+				//remover espaço da ultima palavra
+				if i == (len(args) - 1) {
+					description += txt
+					break
+				}
 				description += fmt.Sprintf("%s ", txt)
 			}
+
 			priority := commom.GetPriority(cmd)
 
 			todo := domain.Todo{
@@ -32,7 +34,7 @@ func Add() *cobra.Command {
 				Priority:    priority,
 			}
 
-			newTodo, err := todoUsecase.CreateTodo(todo)
+			newTodo, err := usecase.CreateTodo(todo)
 
 			if err != nil {
 				fmt.Println(err)
@@ -43,40 +45,5 @@ func Add() *cobra.Command {
 	}
 	addCmd.Flags().BoolP("high", "H", false, "Define a todo normal priority.")
 	addCmd.Flags().BoolP("low", "L", false, "Define a todo low priority.")
-	return addCmd
-}
-
-func Delete() *cobra.Command {
-	addCmd := &cobra.Command{
-		Use:   "remove",
-		Short: "Remove to do",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				fmt.Println("Need todo ID!")
-			}
-
-			id := args[0]
-			err := todoUsecase.DeleteTodo(id)
-
-			if err != nil {
-				fmt.Println(err)
-			}
-		},
-	}
-	return addCmd
-}
-
-func DeleteAll() *cobra.Command {
-	addCmd := &cobra.Command{
-		Use:   "delete",
-		Short: "Delete all to do's",
-		Run: func(cmd *cobra.Command, args []string) {
-			err := todoUsecase.DeleteAllTodos()
-
-			if err != nil {
-				fmt.Println(err)
-			}
-		},
-	}
 	return addCmd
 }
